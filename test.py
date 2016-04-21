1c996c947e3505b1f3a5bac74a930f0960f54705
wpingel <wesley.pingel@gmail.com>
2016-04-20 18:39:53 -0700

    Added files via upload

---
+++ test.py
@@ -0,0 +1,25 @@
+print('Does this even work?')
+a = 4
+"""
+matrix = [[0 for x in range(n)] for x in range(n)]
+	
+for x in range(n):
+	matrix[x][0] = 'S'
+	matrix[0][x] = 'S'
+
+	if x > 0:
+		for y in range(n-1):
+			if matrix[y][y] == 'S':
+				matrix[x][x] = 'Q'
+			elif matrix[y][y] == 'Q':
+				matrix[x][x] = 'U'
+			elif matrix[y][y] == 'U':
+				matrix[x][x] = 'A'
+			elif matrix[y][y] == 'A':
+				matrix[x][x] = 'R'
+			elif matrix[y][y] == 'R':
+				matrix[x][x] = 'E'
+			elif matrix[y][y] == 'E':
+				matrix[x][x] = 'S'
+			else:
+				print('Something terrible has happened.')"""
